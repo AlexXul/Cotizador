a1af7b8a9adbe8d8aaaf9c136f536fa52769abe2
Alvaro Kú <alvaroku123@gmail.com>
2023-01-03 23:17:35 -0600

    migraciones

--- AccesoDatos/Migrations/20230104050628_001.Designer.cs
+++ AccesoDatos/Migrations/20230104050628_001.Designer.cs
@@ -4,14 +4,16 @@ using AccesoDatos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AccesoDatos.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230104050628_001")]
+    partial class _001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
