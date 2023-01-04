fdffc1b1d8cec77fad1c62f27b11a7f51ba42e3c
Alvaro Kú <alvaroku123@gmail.com>
2023-01-03 23:22:19 -0600

    fecha a local mexico

---
+++ Negocios/Utilerias/ConvertidorFecha.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios.Utilerias
+{
+    public static class ConvertidorFecha
+    {
+        public static DateTime ConvertToMexicanDate(DateTime Fecha)
+        {
+            string zonaHoraria = "Central Standard Time (Mexico)";
+            DateTime FechaMexico = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(Fecha, zonaHoraria);
+            return FechaMexico;
+        }
+    }
+}
